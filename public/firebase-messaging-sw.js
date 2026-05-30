016439468116ad4f40b4d11f2b7054dd18d01aa0
davedarbonne <davedarbonne@gmail.com>
2026-05-30 06:43:57 +0600

    Add files via upload

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB73HAl7CIIZnGq6DkZRd4EEjfAoUBUoXw",
+  authDomain: "sweatsquad-85edf.firebaseapp.com",
+  projectId: "sweatsquad-85edf",
+  storageBucket: "sweatsquad-85edf.firebasestorage.app",
+  messagingSenderId: "627088688122",
+  appId: "1:627088688122:web:171c72ca6e512beed5da43"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, {
+    body,
+    icon: '/logo192.png',
+    badge: '/logo192.png',
+    data: payload.data,
+  });
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
