cf9b67bd543727af6e65aa6a431ae582dea15faa
davedarbonne <davedarbonne@gmail.com>
2026-06-07 16:59:13 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -19,3 +19,4 @@ export const db = getFirestore(app);
 export const messaging = getMessaging(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
